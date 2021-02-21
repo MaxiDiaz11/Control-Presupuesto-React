ab02f09869e842b0bc07be924db942c72ed7c145
Maxi Diaz <maxi.diaz66@gmail.com>
2021-02-21 20:30:38 -0300

    se agrego el titulo y un background al body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <div className="py-5 d-flex justify-content-center">
+        <h1 className="titulo text-center d-flex align-items-center zoom py-5 justify-content-center">Control de presupuesto</h1>
+      </div>
+
+    </Fragment>
   );
 }
 
